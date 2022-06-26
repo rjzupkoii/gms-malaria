df68702d3675cd282fdeb6b99442cc9cec9e43d3
rzupko <rzupko@mtu.edu>
2022-06-26 22:17:27 +0000

    Updated imports/shapefiles.js (added 1 line, deleted 3 lines).

--- imports/shapefiles.js
+++ imports/shapefiles.js
@@ -11,12 +11,10 @@ exports.getGms = function() {
   var admin0_names = ee.List(['Cambodia', 'Lao People\'s Democratic Republic', 'Myanmar', 'Thailand', 'Viet Nam']);
   var admin1_names = ee.List(['Guangxi Zhuangzu Zizhiqu', 'Yunnan Sheng']);  
   
-  // Filter on the country names
+  // Filter on the country names, return merged collection
   var countries = ee.FeatureCollection("FAO/GAUL/2015/level0")
     .filter(ee.Filter.inList('ADM0_NAME', admin0_names));
-  
   var regions = ee.FeatureCollection("FAO/GAUL/2015/level1")
     .filter(ee.Filter.inList('ADM1_NAME', admin1_names));  
-    
   return countries.merge(regions);
 };
