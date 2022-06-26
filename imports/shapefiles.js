2fbef7c6fe0b0d599c47c44b6cb2827e54cba2e7
rzupko <rzupko@mtu.edu>
2022-06-26 22:17:15 +0000

    Updated imports/shapefiles.js (added 2 lines, deleted 2 lines).

--- imports/shapefiles.js
+++ imports/shapefiles.js
@@ -8,12 +8,12 @@
 // Get the countries and regions that make up the GMS
 exports.getGms = function() {
   // Official country and region names
-  var admn0_names = ee.List(['Cambodia', 'Lao People\'s Democratic Republic', 'Myanmar', 'Thailand', 'Viet Nam']);
+  var admin0_names = ee.List(['Cambodia', 'Lao People\'s Democratic Republic', 'Myanmar', 'Thailand', 'Viet Nam']);
   var admin1_names = ee.List(['Guangxi Zhuangzu Zizhiqu', 'Yunnan Sheng']);  
   
   // Filter on the country names
   var countries = ee.FeatureCollection("FAO/GAUL/2015/level0")
-    .filter(ee.Filter.inList('ADM0_NAME', admn0_names));
+    .filter(ee.Filter.inList('ADM0_NAME', admin0_names));
   
   var regions = ee.FeatureCollection("FAO/GAUL/2015/level1")
     .filter(ee.Filter.inList('ADM1_NAME', admin1_names));  
