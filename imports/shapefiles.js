e7bc1be6526f8d91f1e642f9b3c71fa7714b0d61
rzupko <rzupko@mtu.edu>
2022-06-26 04:29:03 +0000

    Updated imports/shapefiles.js (added 9 lines).

---
+++ imports/shapefiles.js
@@ -0,0 +1,10 @@
+
+
+exports.getGms = function() {
+  // Offical country names
+  var admn0_names = ee.List(['Cambodia', 'Lao People\'s Democratic Republic', 'Myanmar', 'Thailand', 'Viet Nam']);
+  
+  // Filter on the country names
+  var countries = ee.FeatureCollection("FAO/GAUL/2015/level0")
+    .filter(ee.Filter.inList('ADM0_NAME', admn0_names));  
+};
