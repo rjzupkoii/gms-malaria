ab57ae32782d829def2f98416038bf148b140d54
rzupko <rzupko@mtu.edu>
2022-06-29 03:35:28 +0000

    Updated working.js (added 13 lines, deleted 1 line).

--- working.js
+++ working.js
@@ -3,6 +3,18 @@
 var shapefile = require('users/rzupko/gms-malaria:imports/shapefiles.js');
 var visual = require('users/rzupko/gms-malaria:imports/visualization.js');
 
+// Based upon minimum for full coverage of GMS
+var CLOUD_COVER = 26;
+
+// Get the GMS shapefile
 var gms = shapefile.getGms();
 
+// Filter the Landsat 8 imagery to 2020
+var landsat = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+  .filterBounds(gms)
+  .filterDate('2020-01-01', '2020-12-31')
+  .filterMetadata('CLOUD_COVER', 'less_than', CLOUD_COVER);
+print(landsat);
+
 visual.visualizeGms();
+Map.addLayer(landsat)
