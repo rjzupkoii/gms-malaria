06c478a087ad9ab1bec12ab909a9b3509eaa76ff
rzupko <rzupko@mtu.edu>
2022-06-29 15:56:17 +0000

    Updated working.js (added 2 lines).

--- working.js
+++ working.js
@@ -25,12 +25,14 @@ var getImages = function(item) {
     image.map(maskClouds).mean().clipToCollection(gms));  
 };
 
+// CIR visualization for the GMS
 var viz_gms_cir = {
   'bands' : ['SR_B5', 'SR_B4', 'SR_B3'],
   'min' : 7423.785454545455,
   'max' : 22769.123636363634
 };
 
+// Add the Landsat 8 imagery for the GMS to the map
 var gms = shapefile.getGms();
 var landsat = ee.ImageCollection(gms_wrs2.indicies.map(getImages));
 visual.visualizeGms();
