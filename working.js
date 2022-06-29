65349dd8ff39ef218645fa6a4d14b29be191a539
rzupko <rzupko@mtu.edu>
2022-06-29 15:45:53 +0000

    Updated working.js (added 18 lines, deleted 27 lines).

--- working.js
+++ working.js
@@ -1,41 +1,32 @@
 // Developmental script for new GMS features / scale-up code.
 
+var gms_wrs2 = require('users/rzupko/gms-malaria:assets/gms_wrs2_swaths.js');
 var shapefile = require('users/rzupko/gms-malaria:imports/shapefiles.js');
 var visual = require('users/rzupko/gms-malaria:imports/visualization.js');
 
 // Mask for the cloud and cloud shadow bits
 var CLOUD_MASK = (1 << 3);
 
-// Mask the clouds out of the Landsat 8 image
+// Mask the clouds out of the image
 var maskClouds = function(image) {
-  // Create a mask where the QA masks are set to zero, or clear
   var qa = image.select('QA_PIXEL');
   var mask = qa.bitwiseAnd(CLOUD_MASK).eq(0);
-
-  // Return the masked image
   return image.updateMask(mask);  
 };
 
-function gms_constrained() {
-  // Get the GMS shapefile
-  var gms = shapefile.getGms();
-  Map.centerObject(gms, 5);
-  
-  // Filter the Landsat 8 imagery to 2020
-  var landsat = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
-    .filterBounds(gms)
+var gms = shapefile.getGms();
+var landsat = ee.ImageCollection(gms_wrs2.indicies.map(function(item) {
+    item = ee.List(item);
+    var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+    .filter(ee.Filter.and(
+      ee.Filter.eq('WRS_PATH', item.get(0)),
+      ee.Filter.eq('WRS_ROW', item.get(1))))
     .filterDate('2020-01-01', '2020-12-31');
-  landsat = landsat.map(maskClouds);
-  
-  var viz_cir = {
-    'bands' : ['SR_B5', 'SR_B4', 'SR_B3'],
-    'min': 6131.18,
-    'max': 49339.82
-  };
-  Map.addLayer(landsat, viz_cir, 'Landsat 8, GMS (CIR)');
-}
-
-
-var gms_wrs2_swaths = ee.FeatureCollection('users/rzupko/gms_wrs2_swaths');
-
+  return ee.Image(image.map(maskClouds).mean());
+}));
+landsat = landsat.map(function(image) {
+  return image.clip(gms);
+});
+Map.centerObject(gms);
+Map.addLayer(landsat, [], 'Landsat 8, 2020 (CIR)');
   
