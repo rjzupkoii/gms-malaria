f76c60d3b4c2a00d9856f9836fd07db0e3c73a36
rzupko <rzupko@mtu.edu>
2022-06-29 03:55:01 +0000

    Updated working.js (added 9 lines, deleted 17 lines).

--- working.js
+++ working.js
@@ -6,37 +6,29 @@ var visual = require('users/rzupko/gms-malaria:imports/visualization.js');
 // Based upon minimum for full cloud-masked coverage of GMS
 var CLOUD_COVER = 70;
 
-var SHADOW_MASK = (1 << 3);
-var CLOUD_MASK = (1 << 5);
+// Mask for the cloud and cloud shadow bits
+var MASK = (1 << 3) | (1 << 4);
 
 // Mask the clouds out of the Landsat 8 image
 var maskClouds = function(image) {
-  // Prepare the masks
-
-  
-  // Select the QA band
-  var qa = image.select('QA_PIXEL');
-  
   // Create a mask where the QA masks are set to zero, or clear
-  var mask = qa.bitwiseAnd(SHADOW_MASK).eq(0)
-         .and(qa.bitwiseAnd(CLOUD_MASK).eq(0));
-  
+  var qa = image.select('QA_PIXEL');
+  var mask = qa.bitwiseAnd(MASK).eq(0);
+
   // Return the masked image
   return image.updateMask(mask);  
 };
 
 
 
-visual.visualizeGms();
-
 // Get the GMS shapefile
 var gms = shapefile.getGms();
+Map.centerObject(gms, 5);
 
 // Filter the Landsat 8 imagery to 2020
 var landsat = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
   .filterBounds(gms)
-  .filterDate('2020-01-01', '2020-12-31')
-  .filterMetadata('CLOUD_COVER', 'less_than', CLOUD_COVER);
+  .filterDate('2020-01-01', '2020-12-31');
 landsat = landsat.map(maskClouds);
 
-Map.addLayer(landsat);
+Map.addLayer(landsat, visual.landsatCIR);
