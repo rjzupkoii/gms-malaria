c50728a3c6b02c3e73bbc894611594f7cd786519
rzupko <rzupko@mtu.edu>
2022-07-07 03:14:28 +0000

    Updated imports/processing_wip.js (added 32 lines, deleted 43 lines).

--- imports/processing_wip.js
+++ imports/processing_wip.js
@@ -1,8 +1,24 @@
-// Developmental script for new GMS features / scale-up code.
-var gms_wrs2 = require('users/rzupko/gms-malaria:assets/gms_wrs2_swaths.js');
-var shapefile = require('users/rzupko/gms-malaria:assets/shapefiles.js');
-var visual = require('users/rzupko/gms-malaria:imports/visualization.js');
-
+/*
+ * processing_wip.js
+ *
+ * Work-in-progress replacement for the processing.js script.
+ */
+ 
+// Get the collection of Landsat images that are constrained to the GMS
+exports.getImages = function(wrs2, year) {
+  var load = function(item) {
+    item = ee.List(item);
+    var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+    .filter(ee.Filter.and(
+      ee.Filter.eq('WRS_PATH', item.get(0)),
+      ee.Filter.eq('WRS_ROW', item.get(1))))
+    .filterDate(year + '-01-01', year + '-12-31');
+    return ee.Image(
+      image.map(maskClouds).mean().clipToCollection(gms));  
+  };
+  return ee.ImageCollection(gms_wrs2.indicies.map(load));
+}
+ 
 // Mask for the cloud and cloud shadow bits
 var CLOUD_MASK = (1 << 3);
 
@@ -13,31 +29,4 @@ var maskClouds = function(image) {
   return image.updateMask(mask);  
 };
 
-// Get the collection of Landsat images that are constrained to the GMS
-function getImages(wrs2, year) {
-  var getImages = function(item) {
-    item = ee.List(item);
-    var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
-    .filter(ee.Filter.and(
-      ee.Filter.eq('WRS_PATH', item.get(0)),
-      ee.Filter.eq('WRS_ROW', item.get(1))))
-    .filterDate('2020-01-01', '2020-12-31');
-    return ee.Image(
-      image.map(maskClouds).mean().clipToCollection(gms));  
-  };  
-}
 
-
-// CIR visualization for the GMS
-var viz_gms_cir = {
-  'bands' : ['SR_B5', 'SR_B4', 'SR_B3'],
-  'min' : 7423.785454545455,
-  'max' : 22769.123636363634
-};
-
-// Add the Landsat 8 imagery for the GMS to the map
-var gms = shapefile.getGms();
-var landsat = ee.ImageCollection(gms_wrs2.indicies.map(getImages));
-visual.visualizeGms();
-Map.addLayer(landsat, viz_gms_cir, 'Landsat 8, 2020 (CIR)');
-  
