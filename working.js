0820e5f6993ffa13dd13aaeefa04c6583db354e0
rzupko <rzupko@mtu.edu>
2022-07-07 03:14:42 +0000

    Updated working.js (deleted 24 lines).

--- working.js
+++ working.js
@@ -5,30 +5,6 @@ var visual = require('users/rzupko/gms-malaria:imports/visualization.js');
 
 var processing = requrie('users/rzupko/gms-malaria:imports/processing_wip.js');
 
-// Mask for the cloud and cloud shadow bits
-var CLOUD_MASK = (1 << 3);
-
-// Mask the clouds out of the image
-var maskClouds = function(image) {
-  var qa = image.select('QA_PIXEL');
-  var mask = qa.bitwiseAnd(CLOUD_MASK).eq(0);
-  return image.updateMask(mask);  
-};
-
-// Get the collection of Landsat images that are constrained to the GMS
-function getImages(wrs2, year) {
-  var load = function(item) {
-    item = ee.List(item);
-    var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
-    .filter(ee.Filter.and(
-      ee.Filter.eq('WRS_PATH', item.get(0)),
-      ee.Filter.eq('WRS_ROW', item.get(1))))
-    .filterDate(year + '-01-01', year + '-12-31');
-    return ee.Image(
-      image.map(maskClouds).mean().clipToCollection(gms));  
-  };
-  return ee.ImageCollection(gms_wrs2.indicies.map(load));
-}
 
 
 // CIR visualization for the GMS
