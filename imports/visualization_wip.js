87a3fed5315ce536623c2d0a992ce7a773668180
rzupko <rzupko@mtu.edu>
2022-07-13 04:00:14 +0000

    Updated imports/visualization_wip.js (added 27 lines).

---
+++ imports/visualization_wip.js
@@ -0,0 +1,28 @@
+
+
+// CIR visualization for the GMS
+exports.viz_gms_cir = {
+  'bands' : ['SR_B5', 'SR_B4', 'SR_B3'],
+  'min' : 7423.785454545455,
+  'max' : 22769.123636363634
+};
+
+// Rainfall visualization for the GMS
+exports.viz_rainfall = {
+  'min' : 24.55,
+  'max' : 4497.99,
+  'palette' : ['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494'],
+};
+
+// Temperature visualization for the GMS
+exports.viz_temperature = {
+  'min' : 3.94,  
+  'max' : 38.0,
+  'palette' : [
+    '040274', '040281', '0502a3', '0502b8', '0502ce', '0502e6',
+    '0602ff', '235cb1', '307ef3', '269db1', '30c8e2', '32d3ef',
+    '3be285', '3ff38f', '86e26f', '3ae237', 'b5e22e', 'd6e21f',
+    'fff705', 'ffd611', 'ffb613', 'ff8b13', 'ff6e08', 'ff500d',
+    'ff0000', 'de0101', 'c21301', 'a71001', '911003'
+  ]
+};
