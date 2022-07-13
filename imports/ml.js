04aa5f08433085a2cba4a3daf39f82d501dd94f9
rzupko <rzupko@mtu.edu>
2022-07-13 04:08:45 +0000

    Updated imports/ml.js (added 2 lines, deleted 1 line).

--- imports/ml.js
+++ imports/ml.js
@@ -3,6 +3,7 @@
  *
  * This script contains the data and functions related to machine leanring (ML).
  */
+var features = require('users/rzupko/gms-malaria:imports/features.js');
 
 // Landsat 8 bands that are used for classification
 exports.classifiedBands = ['SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7'];
