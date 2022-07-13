35d7a89da07138108d45db2c0708b320598581b3
rzupko <rzupko@mtu.edu>
2022-07-13 04:02:58 +0000

    Updated working.js (added 15 lines, deleted 43 lines).

--- working.js
+++ working.js
@@ -2,40 +2,13 @@
 var gms_wrs2 = require('users/rzupko/gms-malaria:assets/gms_wrs2_swaths.js');
 var shapefile = require('users/rzupko/gms-malaria:assets/shapefiles.js');
 
-var features = requrie('users/rzupko/gms-malaria:imports/features.js');
+// Assets that need to be migrated
+var features = require('users/rzupko/gms-malaria:imports/features.js');
+
+// Work-in-progress imports
 var processing = require('users/rzupko/gms-malaria:imports/processing_wip.js');
-var visual = require('users/rzupko/gms-malaria:imports/visualization.js');
+var visual = require('users/rzupko/gms-malaria:imports/visualization_wip.js');
 
-// CIR visualization for the GMS
-var viz_gms_cir = {
-  'bands' : ['SR_B5', 'SR_B4', 'SR_B3'],
-  'min' : 7423.785454545455,
-  'max' : 22769.123636363634
-};
-
-var viz_rainfall = {
-  'min' : 24.55,
-  'max' : 4497.99,
-  'palette' : ['#ffffcc','#a1dab4','#41b6c4','#2c7fb8','#253494'],
-};
-
-// Temperature visualization
-var viz_temperature = {
-  'min' : 3.94,  
-  'max' : 38.0,
-  'palette' : [
-    '040274', '040281', '0502a3', '0502b8', '0502ce', '0502e6',
-    '0602ff', '235cb1', '307ef3', '269db1', '30c8e2', '32d3ef',
-    '3be285', '3ff38f', '86e26f', '3ae237', 'b5e22e', 'd6e21f',
-    'fff705', 'ffd611', 'ffb613', 'ff8b13', 'ff6e08', 'ff500d',
-    'ff0000', 'de0101', 'c21301', 'a71001', '911003'
-  ] };
-  
-var viz_bounds = {
-  'min' : 0,
-  'max' : 366,
-  'palette' : ['#2f942e', '#b9191e'],
-};
 
 var classifiedBands = ['SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7'];
 
@@ -49,18 +22,16 @@ function getClassifier() {
       ee.Filter.eq('WRS_ROW', 50)))
     .filterDate('2020-01-21', '2020-01-23')
     .mean();
-  var polygons = features.getFeatures();
-  
-  
-  // Sample the input imagery
+
+  // Sample the labeled features
   var training = labeled.select(bands).sampleRegions({
-    collection: polygons,
+    collection: features.getFeatures(),
     properties: ['class'],
     scale: 30
   });
   
-  // Make a SVM classifier and train it
-  var classifier = ee.Classifier.libsvm().train({
+  // Make a CART classifier, train it, and return the object
+  return ee.Classifier.smileCart().train({
     features: training,
     classProperty: 'class',
     inputProperties: bands
@@ -68,15 +39,16 @@ function getClassifier() {
 }
 
 
-// TODO Update this with cloudmasking
-// Get the training image
-var training = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
   .filter(ee.Filter.and(
     ee.Filter.eq('WRS_PATH', 125),
     ee.Filter.eq('WRS_ROW', 50)))
   .filterDate('2020-01-21', '2020-01-23')
   .mean();
-Map.addLayer(training)
+
+var classifier = getClassifier();
+var classified = image.select(classifiedBands).classify(classifier);
+Map.addLayer(classified, visual.viz_trainingPalette, 'Classified');
 
 
 // Placeholder, will be returned by the UI
