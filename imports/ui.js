235609cc83ec7babbab620a6147d38392ee592af
rzupko <rzupko@mtu.edu>
2022-07-17 20:16:31 +0000

    Updated imports/ui.js (added 16 lines).

--- imports/ui.js
+++ imports/ui.js
@@ -11,6 +11,22 @@ exports.prepareUI = function() {
   Map.add(getIntermediateCheckbox());
 };
 
+// Add a layer to the map with the GMS outlined
+exports.visualizeGms = function() {
+  // Load the GMS borders and generate the outlines
+  var gms = shapefiles.getGms();
+  var empty = ee.Image().byte();
+  var outline = empty.paint({
+    featureCollection: gms,
+    color: 1,
+    width: 0.5,
+  });
+  
+  // Update the map
+  Map.centerObject(gms, 5);
+  Map.addLayer(outline, { palette: '#757575' }, 'Greater Mekong Subregion');
+};
+
 // Return a checkbox that toggles environmental maps
 function getEnvironmentalCheckbox(){
   return ui.Checkbox({
