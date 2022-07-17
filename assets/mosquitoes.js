5d21ba6bcc401d246ba78ebadf8b9ab84f40460f
rzupko <rzupko@mtu.edu>
2022-07-17 23:53:04 +0000

    Updated assets/mosquitoes.js (added 6 lines, deleted 7 lines).

--- assets/mosquitoes.js
+++ assets/mosquitoes.js
@@ -4,9 +4,8 @@
  * This asset file contains data for various Anopheles genus mosquitoes.
  */
 
-// TODO Add the aestivation periods instead of the placeholder values
-exports.aDirus = {
-  'species'  : 'A. dirus',
+exports.aBaimaii = {
+  'species'  : 'A. baimaii',
   'rainfall' : 1500,          // Obsomer et al. 2012
   'tempMin'  : 11.0,
   'tempMean' : 20.0,          
@@ -17,16 +16,16 @@ exports.aDirus = {
 
 exports.aCrascens = {
   'species'  : 'A. crascens',
-  'rainfall' : 2000,          // Obsomer et al. 2012
-  'tempMin'  : 20.0,
+  'rainfall' : 1200,          // Obsomer et al. 2012
+  'tempMin'  : 12.5,
   'tempMean' : 25.0,          
   'tempMax'  : 28.0,
   'lifeExpectancy' : 31,      // Placeholder - Wikipedia -> CDC
   'aestivationMax' : 212      //Lehmann et al. 2010
 };
 
-exports.aBaimaii = {
-  'species'  : 'A. baimaii',
+exports.aDirus = {
+  'species'  : 'A. dirus',
   'rainfall' : 1500,          // Obsomer et al. 2012
   'tempMin'  : 11.0,
   'tempMean' : 20.0,          
