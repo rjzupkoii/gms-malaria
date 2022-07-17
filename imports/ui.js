40d3b44ee81d39026f0ed870336e654adb6af7a7
rzupko <rzupko@mtu.edu>
2022-07-17 02:26:12 +0000

    Updated imports/ui.js (added 12 lines, deleted 10 lines).

--- imports/ui.js
+++ imports/ui.js
@@ -1,13 +1,15 @@
 
 
 // Return a checkbox that toggles intermediate maps
-exports.showIntermediate = ui.Checkbox({
-  label: 'Show Intermediate Maps',
-  onChange: function(checked) {
-    Map.layers().get(4).setShown(checked);  // A. dirus / Days Outside Bounds
-    Map.layers().get(5).setShown(checked);  // Landcover
-  },
-  style: {
-    position: 'top-right'
-  }
-});
+exports.getIntermediateCheckbox = function() {
+  return ui.Checkbox({
+    label: 'Show Intermediate Maps',
+    onChange: function(checked) {
+      Map.layers().get(4).setShown(checked);  // A. dirus / Days Outside Bounds
+      Map.layers().get(5).setShown(checked);  // Landcover
+    },
+    style: {
+      position: 'top-right'
+    }
+  });
+};
