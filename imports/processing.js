dec2b102ba124580ef9e5b84c0be1cba3e392fad
rzupko <rzupko@mtu.edu>
2022-07-17 19:06:19 +0000

    Updated imports/processing.js (added 1 line).

--- imports/processing.js
+++ imports/processing.js
@@ -10,6 +10,7 @@ exports.getAnnualRainfall = function(aoi, year) {
   var collection = ee.ImageCollection('UCSB-CHG/CHIRPS/PENTAD')
     .filterDate(year + '-01-01', year + '-12-31');
   var results = collection.reduce(ee.Reducer.sum()).rename('total_rainfall');
+  print(results)
   results = results.addBand(collection.reduce(ee.Reducer.mean()).rename('mean_rainfall'));
   return results.clip(aoi);
 };
