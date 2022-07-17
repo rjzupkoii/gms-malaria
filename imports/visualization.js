1d903aa83067dae206a45571fa1378b32be1a3e0
rzupko <rzupko@mtu.edu>
2022-07-17 19:44:46 +0000

    Updated imports/visualization.js (added 1 line, deleted 1 line).

--- imports/visualization.js
+++ imports/visualization.js
@@ -51,7 +51,7 @@ exports.viz_temperature = {
 exports.viz_habitatPalette = { 
   min: 0, 
   max: 3, 
-  palette: ['blue', 'yellow', 'orange', 'red'] 
+  palette: ['#bdbdbd', '#fee8c8', '#fdbb84', '#e34a33'] 
 };
 
 // Color palette for the training data, note that Deep
