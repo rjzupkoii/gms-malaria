650ad0f7b2237d3adb56ce48baec9adb9bcb4da8
rzupko <rzupko@mtu.edu>
2022-07-17 21:32:12 +0000

    Updated landing.js (added 3 lines, deleted 27 lines).

--- landing.js
+++ landing.js
@@ -53,33 +53,9 @@ var habitat = processing.getHabitat({
     'aestivationMax'     : species.aestivationMax
 });
 
-function riskAssessment(landcover, habitat) {
-  // Generate the buffer based upon the land cover type, using cumulative cost  for the
-  // buffer isn't exactly the same as a buffer, but results in the same effect
-  var buffer = ee.Image(1).cumulativeCost({
-    source: landcover.gte(20),                  // Development (20) or Agricultural (21)
-    maxDistance: 1500,                          // Obsomer et al. 2007, "very high" density
-  }).lt(1500);
-  var high = habitat.gt(0).and(landcover.mask(buffer).gte(20));
 
-  buffer = ee.Image(1).cumulativeCost({
-    source: landcover.gte(20), 
-    maxDistance: 5000,                        // Obsomer et al. 2007, moderate density
-  }).lt(5000);
-  var moderate = habitat.gt(0).and(landcover.mask(buffer).gte(20));
-  
-  // Our base risk is when we are within the habitat window and forest/vegitation is present
-  var base = habitat.gt(0).and(landcover.eq(11).or(landcover.eq(12)));
-  
-  // Return the total across the three layers
-  return ee.Image(0).expression('base + moderate + high', {
-    base: base,
-    high: high.gt(0).unmask(),
-    moderate: moderate.gt(0).unmask()
-  });
-}
 
-var risk = riskAssessment(landcover, habitat);
+var risk = processing.riskAssessment(landcover, habitat);
 
 // Add the enviornmental and intermediate data to the UI, note it is off by default
 Map.addLayer(environmental.select('total_rainfall'), visual.viz_rainfall, 'Total Annual Rainfal, CHIRPS/PENTAD', false);
@@ -88,5 +64,5 @@ Map.addLayer(intermediate.select('days_outside_bounds'), visual.viz_bounds, 'A. 
 Map.addLayer(landcover, visual.viz_trainingPalette, 'Landcover', false);
 
 // Add the species and malaria risk layers
-Map.addLayer(habitat, visual.viz_habitatPalette, 'A. dirus / Probable Habitat', false);
+Map.addLayer(habitat, visual.viz_habitatPalette, 'A. dirus / Probable Habitat');
 Map.addLayer(risk, visual.vis_riskPalette, 'A. dirus / Malaria Risk');
