462ff12f9d4169962da781c3b2e9a60a181ce9c7
rzupko <rzupko@mtu.edu>
2022-07-17 02:27:29 +0000

    Updated imports/ui.js (added 15 lines, deleted 1 line).

--- imports/ui.js
+++ imports/ui.js
@@ -1,5 +1,19 @@
 
 
+// Return a checkbox that toggles environmental maps
+exports.getEnvironmentalCheckbox = function() {
+  return ui.Checkbox({
+    label: 'Show Environmental Maps',
+    onChange: function(checked) {
+      Map.layers().get(2).setShown(checked);  // CHIRPS/PENTAD
+      Map.layers().get(3).setShown(checked);  // MOD11A1.061
+    },
+    style: {
+      position: 'top-right'
+    }
+  });
+};
+
 // Return a checkbox that toggles intermediate maps
 exports.getIntermediateCheckbox = function() {
   return ui.Checkbox({
