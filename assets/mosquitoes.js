46b9538c2048e64ac3a59fd65c4489e4d88ed819
rzupko <rzupko@mtu.edu>
2022-07-17 23:51:46 +0000

    Updated assets/mosquitoes.js (added 20 lines).

--- assets/mosquitoes.js
+++ assets/mosquitoes.js
@@ -14,3 +14,23 @@ exports.aDirus = {
   'lifeExpectancy' : 31,      // Placeholder - Wikipedia -> CDC
   'aestivationMax' : 212      //Lehmann et al. 2010
 };
+
+exports.aCrascens = {
+  'species'  : 'A. crascens',
+  'rainfall' : 2000,          // Obsomer et al. 2012
+  'tempMin'  : 20.0,
+  'tempMean' : 25.0,          
+  'tempMax'  : 28.0,
+  'lifeExpectancy' : 31,      // Placeholder - Wikipedia -> CDC
+  'aestivationMax' : 212      //Lehmann et al. 2010
+};
+
+exports.aDirus = {
+  'species'  : 'A. dirus',
+  'rainfall' : 1500,          // Obsomer et al. 2012
+  'tempMin'  : 11.0,
+  'tempMean' : 20.0,          
+  'tempMax'  : 28.0,
+  'lifeExpectancy' : 31,      // Placeholder - Wikipedia -> CDC
+  'aestivationMax' : 212      //Lehmann et al. 2010
+};
