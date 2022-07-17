3378b605097bc14f4b4f2ac9ad58d094382277b5
rzupko <rzupko@mtu.edu>
2022-07-17 19:59:33 +0000

    Updated landing.js (added 36 lines, deleted 1 line).

--- landing.js
+++ landing.js
@@ -53,13 +53,48 @@ var habitat = processing.getHabitat({
     'aestivationMax'     : species.aestivationMax
 });
 
+function riskAssessment(landcover, habitat) {
+  // Generate the 1 km buffer based upon the land cover type, using cumulative 
+  // cost for the buffer isn't exactly the same as a buffer, but results in
+  // the same effect
+  var buffer = ee.Image(0).expression('landcover >= 20', {landcover: landcover});
+  buffer = ee.Image(1).cumulativeCost({
+    source: buffer, 
+    maxDistance: 1000,
+  }).lt(1000);
+  var masked = habitat.updateMask(buffer);
+  
+  // High risk are areas where humans likely live along side mosquitos
+//  var high = ee.Image(0).expression('(landcover >= 20) && (habitat > 1)', {landcover: landcover, habitat: habitat});
+  var high = ee.Image(0).expression('masked > 1 ? 1 : 0', {masked: masked});      
+  
+  // Generate the 5 km buffer based upon the land cover type
+  buffer = ee.Image(0).expression('landcover >= 20', {landcover: landcover});
+  buffer = ee.Image(1).cumulativeCost({
+    source: buffer, 
+    maxDistance: 5000,
+  }).lt(5000);
+  masked = habitat.updateMask(buffer);
+  
+  // Moderate risk is mosquito habitat with 5km of humans, 
+  // note ternary operator to force a value with the mask 
+  var moderate = ee.Image(0).expression('masked > 1 ? 1 : 0', {masked: masked});      
+  
+  // Low risk is mosquito habitat
+  var low = ee.Image(0).expression('habitat > 1', {habitat: habitat});
+  
+  // Return categorized risk
+  return ee.Image(0).expression('high + moderate + low', {high: high, moderate: moderate, low: low});  
+}
+
+var risk = risk
 
 // Add everything to the UI
 Map.addLayer(environmental.select('total_rainfall'), visual.viz_rainfall, 'Total Annual Rainfal, CHIRPS/PENTAD', false);
 Map.addLayer(environmental.select('mean_temperature'), visual.viz_temperature, 'Mean Temperature, MOD11A1.061', false);
 Map.addLayer(intermediate.select('days_outside_bounds'), visual.viz_bounds, 'A. dirus / Days Outside Bounds', false);
 Map.addLayer(landcover, visual.viz_trainingPalette, 'Landcover', false);
-Map.addLayer(habitat, visual.viz_habitatPalette, 'A. dirus / Probable Habitat');
+Map.addLayer(habitat, visual.viz_habitatPalette, 'A. dirus / Probable Habitat', false);
 
 
 
