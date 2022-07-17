5ddf4ae6dcddf2462a9185065731fcc4b087a228
rzupko <rzupko@mtu.edu>
2022-07-17 04:42:01 +0000

    Updated imports/visualization.js (added 2 lines, deleted 2 lines).

--- imports/visualization.js
+++ imports/visualization.js
@@ -99,9 +99,9 @@ exports.viz_trainingPalette = {
     'deeppink', 
     'deeppink',
     'blue',         // 10, Water
-    'darkgreen',    // 11, Forest
+    'green',        // 11, Forest
     'green',        // 12, Vegetation
-    'darkseagreen', // 13, Vegetation / Scrub
+    'green',        // 13, Vegetation / Scrub
     'brown',        // 14, Barren
     'deeppink', 
     'deeppink', 
