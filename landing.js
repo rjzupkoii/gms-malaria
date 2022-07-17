4d8f4ab5f8cc3bb229a91202a311802a62567372
rzupko <rzupko@mtu.edu>
2022-07-17 19:11:48 +0000

    Updated landing.js (added 28 lines, deleted 8 lines).

--- landing.js
+++ landing.js
@@ -34,13 +34,33 @@ Map.addLayer(landsat, visual.viz_gms_cir, 'Landsat 8, 2020 (CIR)', false);
 Map.addLayer(landsat, visual.viz_gms_rgb, 'Landsat 8, 2020');
 
 // Prepare the environmental and intermediate data needed
-var environmental = processing.getAnnualRainfall(gms, year).rename('rainfall');
-environmental = environmental.addBands(processing.getMeanTemperature(gms, year).rename('temperature'));
-var intermediate = processing.getTemperatureBounds(gms, year, species.tempMin, species.tempMax).rename('bounds');
-var landcover = ml.classify(landsat);
+var environmental = processing.getAnnualRainfall(gms, year);
+environmental = environmental.addBands(processing.getMeanTemperature(gms, year));
+// var intermediate = processing.getTemperatureBounds(gms, year, species.tempMin, species.tempMax).rename('bounds');
+// var landcover = ml.classify(landsat);
 
 // Add everything to the UI
-Map.addLayer(environmental.select('rainfall'), visual.viz_rainfall, 'Annual Rainfal, CHIRPS/PENTAD', false);
-Map.addLayer(environmental.select('temperature'), visual.viz_temperature, 'Mean Temperature, MOD11A1.061', false);
-Map.addLayer(intermediate, visual.viz_bounds, 'A. dirus / Days Outside Bounds', false);
-Map.addLayer(landcover, visual.viz_trainingPalette, 'Landcover', false);
+Map.addLayer(environmental.select('total_rainfall'), visual.viz_rainfall, 'Total Annual Rainfal, CHIRPS/PENTAD', false);
+Map.addLayer(environmental.select('mean_temperature'), visual.viz_temperature, 'Mean Temperature, MOD11A1.061', false);
+// Map.addLayer(intermediate, visual.viz_bounds, 'A. dirus / Days Outside Bounds', false);
+// Map.addLayer(landcover, visual.viz_trainingPalette, 'Landcover', false);
+
+// // Classify the habitat based upon the inputs
+//   var variables = {
+//     rainfall: inputs.rainfall, 
+//     minRainfall: inputs.annualRainfall, 
+//     temperature: inputs.temperature, 
+//     minTemp: inputs.minimumMeanTemperature,
+//     bounds: inputs.bounds};
+  
+//   // Primary habitat is completely within the environmental envelope
+//   var primary = ee.Image(0).expression('(rainfall > minRainfall) && (temperature >= minTemp) && (bounds == 0)', variables);
+    
+//   // Secondary is within the envelope for the life expectancy of an active female (43 days)
+//   var secondary = ee.Image(0).expression('(rainfall > minRainfall) && (temperature >= minTemp) && (bounds < 43)', variables);
+    
+//   // Tertiary is within the envelope for the dormant life expectancy of a female (180 days)
+//   var tertiary = ee.Image(0).expression('(rainfall > minRainfall) && (temperature >= minTemp) && (bounds < 180)', variables);
+  
+//   // Merge the classifications and return
+//   var habitat = ee.Image(0).expression('primary + secondary + tertiary', {primary: primary, secondary: secondary, tertiary: tertiary});
