4d189af9ecde83fceb002063a2ed505412e12ecd
rzupko <rzupko@mtu.edu>
2022-07-17 02:43:58 +0000

    Updated imports/ml.js (added 36 lines, deleted 72 lines).

--- imports/ml.js
+++ imports/ml.js
@@ -4,12 +4,6 @@
  * This script contains the data and functions related to machine leanring (ML).
  */
 var features = require('users/rzupko/gms-malaria:imports/features.js');
-/*
- * ml.js
- *
- * This script contains the data and functions related to machine leanring (ML).
- */
-var features = require('users/rzupko/gms-malaria:imports/features.js');
 var processing = require('users/rzupko/gms-malaria:imports/processing_wip.js');
 
 // Landsat 8 bands that are used for classification
@@ -25,36 +19,6 @@ exports.getClassifier = function() {
       ee.Filter.eq('WRS_PATH', 125),
       ee.Filter.eq('WRS_ROW', 50)))
     .filterDate('2020-01-21', '2020-01-23')
-    .first();
-
-  // Sample the labeled features
-  var training = image.select(this.classifiedBands).sampleRegions({
-    collection: features.getFeatures(),
-    properties: ['class'],
-    scale: 30
-  });
-  
-  // Make a CART classifier, train it, and return the object
-  return ee.Classifier.smileCart().train({
-    features: training,
-    classProperty: 'class',
-    inputProperties: this.classifiedBands
-  });  
-};
-
-// Landsat 8 bands that are used for classification
-exports.classifiedBands = ['SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7'];
-
-// Get the trained classifer that will be used to determine the landcover class
-exports.getClassifier = function() {
-  // TODO Cloud filtering, improve training data
-  
-  // Load the image for classification
-  var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
-    .filter(ee.Filter.and(
-      ee.Filter.eq('WRS_PATH', 125),
-      ee.Filter.eq('WRS_ROW', 50)))
-    .filterDate('2020-01-01', '2020-12-31')
     .first();
 
   // Sample the labeled features
