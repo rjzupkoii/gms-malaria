5ec3271b4b5323dac669ffefa5b167a813f6c157
rzupko <rzupko@mtu.edu>
2022-07-17 18:52:09 +0000

    Updated imports/ml.js (added 1 line, deleted 1 line).

--- imports/ml.js
+++ imports/ml.js
@@ -12,7 +12,7 @@ exports.classifiedBands = ['SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7']
 // Return the classified landcover for the region provided
 exports.classify = function(indicies, region, year) {
   var classifier = ml.getClassifier(features.getFeatures());  
-  var landsat = processing.getImages(indicies, gms, year);
+  var landsat = processing.getImages(indicies, region, year);
   var classified = landsat.map(function(image) {
     return image.select(ml.classifiedBands).classify(classifier);
   });  
