fee4cd5dc17b7c5e74e2e9eb24a142070c44ab60
rzupko <rzupko@mtu.edu>
2022-07-18 00:59:49 +0000

    Updated landing.js (deleted 10 lines).

--- landing.js
+++ landing.js
@@ -5,16 +5,6 @@
  * Landsat imagery to generate malaria risk rasters based upon land 
  * classification and ecological characteristics.
  */
- 
-// Import the various assets that we need
-var gms_wrs2 = require('users/rzupko/gms-malaria:assets/gms_wrs2_swaths.js');
-var mosquitoes = require('users/rzupko/gms-malaria:assets/mosquitoes.js');
-var shapefile = require('users/rzupko/gms-malaria:assets/shapefiles.js');
-var visual = require('users/rzupko/gms-malaria:assets/visualization.js');
-
-// Import the various functional scripts
-var ml = require('users/rzupko/gms-malaria:imports/ml.js');
-var processing = require('users/rzupko/gms-malaria:imports/processing.js');
 var gmsUi = require('users/rzupko/gms-malaria:imports/ui.js');
 
 // Prepare the UI
