7648d3cf61506933acf8949dd6c784881495ab99
rzupko <rzupko@mtu.edu>
2022-07-18 00:18:14 +0000

    Updated landing.js (added 51 lines, deleted 49 lines).

--- landing.js
+++ landing.js
@@ -17,10 +17,6 @@ var ml = require('users/rzupko/gms-malaria:imports/ml.js');
 var processing = require('users/rzupko/gms-malaria:imports/processing.js');
 var gmsUi = require('users/rzupko/gms-malaria:imports/ui.js');
 
-// Placeholders, these will (eventually) be managed by the UI itself
-var year = '2020';
-var species = mosquitoes.aDirus;
-
 // Prepare the UI
 gmsUi.prepareUI();
 
@@ -37,48 +33,54 @@ var select = ui.Select({
 
 Map.add(select);
 
+function refresh(year, species) {
+  // Next add the base Landsat layers
+  var landsat = processing.getImages(gms_wrs2.indices, gms, year);
+  Map.addLayer(landsat, visual.viz_gms_cir, 'Landsat 8, 2020 (CIR)', false);
+  Map.addLayer(landsat, visual.viz_gms_rgb, 'Landsat 8, 2020');
+  
+  // Process the data that only changes based on the year
+  var environmental = processing.getAnnualRainfall(gms, year);
+  environmental = environmental.addBands(processing.getMeanTemperature(gms, year));
+  var landcover = ml.classify(landsat);
+  
+  // Process the data that changes based upon the species selected
+  var intermediate = processing.getTemperatureBounds(gms, year, species.tempMin, species.tempMax);
+  
+  // Classify the habitat based upon the inputs
+  var habitat = processing.getHabitat({
+      // Raster data
+      'totalRainfall'      : environmental.select('total_rainfall'),
+      'meanTemperature'    : environmental.select('mean_temperature'),
+      'daysOutsideBounds'  : intermediate.select('days_outside_bounds'),
+      
+      // Species data
+      'speciesRainfall'    : species.rainfall,
+      'speciesTemperature' : species.tempMin,
+      'speciesLife'        : species.lifeExpectancy,
+      'aestivationMax'     : species.aestivationMax
+  });
+  
+  // Prepare the risk assessment based upon the landcover and habitat
+  var risk = processing.getRiskAssessment(landcover, habitat);
+  
+  // Base data that only needs to be done once for the year selected
+  Map.addLayer(environmental.select('total_rainfall'), visual.viz_rainfall, 'Total Annual Rainfal, CHIRPS/PENTAD', false);
+  Map.addLayer(environmental.select('mean_temperature'), visual.viz_temperature, 'Mean Temperature, MOD11A1.061', false);
+  Map.addLayer(landcover, visual.viz_trainingPalette, 'Classified Landcover', false);
+  
+  // Intermediate data for the Anopheles genus selected
+  Map.addLayer(intermediate.select('days_outside_bounds'), visual.viz_bounds, 'Anopheles / Days Outside Bounds', false);
+  
+  // Anopheles genus specific data
+  Map.addLayer(habitat, visual.viz_habitatPalette, 'Anopheles / Probable Habitat');
+  Map.addLayer(risk, visual.vis_riskPalette, 'Anopheles  / Malaria Risk');  
+}
+
 // Start by drawing the GMS
 var gms = shapefile.getGms();
 gmsUi.visualizeGms();
 
-// Next add the base Landsat layers
-var landsat = processing.getImages(gms_wrs2.indices, gms, year);
-Map.addLayer(landsat, visual.viz_gms_cir, 'Landsat 8, 2020 (CIR)', false);
-Map.addLayer(landsat, visual.viz_gms_rgb, 'Landsat 8, 2020');
+refresh('2020', mosquitoes.aDirus);
 
-// Process the data that only changes based on the year
-var environmental = processing.getAnnualRainfall(gms, year);
-environmental = environmental.addBands(processing.getMeanTemperature(gms, year));
-var landcover = ml.classify(landsat);
 
-// Process the data that changes based upon the species selected
-var intermediate = processing.getTemperatureBounds(gms, year, species.tempMin, species.tempMax);
-
-// Classify the habitat based upon the inputs
-var habitat = processing.getHabitat({
-    // Raster data
-    'totalRainfall'      : environmental.select('total_rainfall'),
-    'meanTemperature'    : environmental.select('mean_temperature'),
-    'daysOutsideBounds'  : intermediate.select('days_outside_bounds'),
-    
-    // Species data
-    'speciesRainfall'    : species.rainfall,
-    'speciesTemperature' : species.tempMin,
-    'speciesLife'        : species.lifeExpectancy,
-    'aestivationMax'     : species.aestivationMax
-});
-
-// Prepare the risk assessment based upon the landcover and habitat
-var risk = processing.getRiskAssessment(landcover, habitat);
-
-// Base data that only needs to be done once for the year selected
-Map.addLayer(environmental.select('total_rainfall'), visual.viz_rainfall, 'Total Annual Rainfal, CHIRPS/PENTAD', false);
-Map.addLayer(environmental.select('mean_temperature'), visual.viz_temperature, 'Mean Temperature, MOD11A1.061', false);
-Map.addLayer(landcover, visual.viz_trainingPalette, 'Classified Landcover', false);
-
-// Intermediate data for the Anopheles genus selected
-Map.addLayer(intermediate.select('days_outside_bounds'), visual.viz_bounds, 'Anopheles / Days Outside Bounds', false);
-
-// Anopheles genus specific data
-Map.addLayer(habitat, visual.viz_habitatPalette, 'Anopheles / Probable Habitat');
-Map.addLayer(risk, visual.vis_riskPalette, 'Anopheles  / Malaria Risk');
