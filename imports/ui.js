e8d25f2d64231eabaa5ef37825082ddd2c9de243
rzupko <rzupko@mtu.edu>
2022-07-18 01:25:39 +0000

    Updated imports/ui.js (added 3 lines, deleted 1 line).

--- imports/ui.js
+++ imports/ui.js
@@ -33,8 +33,10 @@ exports.prepareUI = function() {
       ui.Label(' '),
       ui.Label(' '),
       ui.Label(' '),
-      ui.Label('The environmental maps are those used to assess the environmental suitablity for the selected species'),
+      ui.Label('The environmental maps those used to assess the environmental suitablity for the selected species.'),
       getEnvironmentalCheckbox(),
+      ui.Label(' '),
+      ui.Label('Intermediate maps include the days outside the species envelope, and landcover for the current year.'),
       getIntermediateCheckbox(),
     ], 
     'flow', { 'width' : '250px' });
