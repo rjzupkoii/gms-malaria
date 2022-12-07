9c0e0ea493202e490d6bae23f3a762b0c07a1481
rzupko <rzupko@mtu.edu>
2022-12-07 16:58:43 +0000

    Updated assets/landsat.js (added 9 lines).

--- assets/landsat.js
+++ assets/landsat.js
@@ -3,3 +3,12 @@
  *
  * Wrapper for the Landsat imagery since both Landsat 7 and Landsat 8 are supported.
  */
+ 
+exports.LandsatType = {
+  Landsat7 : 'LANDSAT/LE07/C02/T1_L2',
+  Landsat8 : 'LANDSAT/LC08/C02/T1_L2',
+};
+ 
+exports.getVisualizationRGB = function(type) {
+  
+}
