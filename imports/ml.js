aba3fefaf67dccdc77985f538025a52918420a34
rzupko <rzupko@mtu.edu>
2022-12-07 18:18:20 +0000

    Updated imports/ml.js (added 3 lines).

--- imports/ml.js
+++ imports/ml.js
@@ -9,6 +9,9 @@ var processing = require('users/rzupko/gms-malaria:imports/processing.js');
 
 // Return the classified landcover for the region provided
 exports.classify = function(imagery, year) {
+  var satellite = landsate.getSatellite(year);
+  print(satellite)
+  
   var classifier = exports.getClassifier(features.getFeatures(), year);  
   var classified = imagery.map(function(image) {
     return image.select(exports.classifiedBands).classify(classifier);
