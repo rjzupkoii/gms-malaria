e568c9507a761c6b4e244b2875a0e60ef6ea2e8c
rzupko <rzupko@mtu.edu>
2022-12-08 20:16:33 +0000

    Updated assets/mosquitoes.js (added 29 lines, deleted 35 lines).

--- assets/mosquitoes.js
+++ assets/mosquitoes.js
@@ -7,47 +7,41 @@
  */
 
 exports.aBaimaii = {
-  'species'  : 'A. baimaii',
-  'rainfall' : 1200,
-  'tempMin'  : 12.5,
-  'tempMean' : 24.0,          
-  'tempMax'  : 28.0,
-  'lifeExpectancy' : 31,
+  'species'         : 'A. baimaii',
+  'rainfall'        : 1200,
+  'tempMin'         : 12.5,
+  'tempMeanLB'      : 24.0,          
+  'tempMeanUB'      : 27.5,
+  'tempMax'         : 28.0,
+  'lifeExpectancy'  : 31,
 };
 
 exports.aCrascens = {
-  'species'  : 'A. crascens',
-  'rainfall' : 2000,
-  'tempMin'  : 20.0,
-  'tempMean' : 25.0,          
-  'tempMax'  : 28.0,
-  'lifeExpectancy' : 31,
+  'species'         : 'A. crascens',
+  'rainfall'        : 2000,
+  'tempMin'         : 20.0,
+  'tempMeanLB'      : 25.0,          
+  'tempMeanUB'      : 26.0,
+  'tempMax'         : 28.0,
+  'lifeExpectancy'  : 31,
 };
 
 exports.aDirus = {
-  'species'     : 'A. dirus s.s.',
-  'rainfall'    : 1500,
-  'tempMin'     : 12.5,
-  'tempMeanLB'  : 24.0,          
-  'tempMeanUB'  : 27.5,
-  'tempMax'     : 26.0,
-  'lifeExpectancy' : 31,
-};
-
-exports.aNemophilous = {
-  'species'  : 'A. nemophilous',
-  'rainfall' : 1500,
-  'tempMin'  : 14.0,
-  'tempMean' : 20.0,          
-  'tempMax'  : 26.0,
-  'lifeExpectancy' : 31,
+  'species'         : 'A. dirus s.s.',
+  'rainfall'        : 1500,
+  'tempMin'         : 12.5,
+  'tempMeanLB'      : 24.0,          
+  'tempMeanUB'      : 27.5,
+  'tempMax'         : 26.0,
+  'lifeExpectancy'  : 31,
 };
 
 exports.aScanloni = {
-  'species'  : 'A. scanloni',
-  'rainfall' : 1500,
-  'tempMin'  : 15.0,
-  'tempMean' : 25.0,          
-  'tempMax'  : 28.0,
-  'lifeExpectancy' : 31,
+  'species'         : 'A. scanloni',
+  'rainfall'        : 1500,
+  'tempMin'         : 15.0,
+  'tempMeanLB'      : 25.0,          
+  'tempMeanUB'      : 26.0,     
+  'tempMax'         : 28.0,
+  'lifeExpectancy'  : 31,
 };
