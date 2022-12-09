4bcc81561ef43e56f1008058fe403979bdd01129
rzupko <rzupko@mtu.edu>
2022-12-09 17:46:55 +0000

    Updated imports/widgets.js (added 48 lines, deleted 1 line).

---
+++ imports/widgets.js
@@ -0,0 +1,48 @@
+/*
+ * widgets.js
+ *
+ * Generalized UI widgets.
+ */
+
+function createColorBar(titleText, palette, min, max) {
+  // Prepare the title
+  var title = ui.Label({
+    value: titleText, 
+    style: {fontWeight: 'bold', textAlign: 'center', stretch: 'horizontal'}});
+
+  // Prepare the color bar
+  var colorBar = ui.Thumbnail({
+    image: ee.Image.pixelLonLat().select(0),
+    params: makeColorBarParams(vis.palette),
+    style: {stretch: 'horizontal', margin: '0px 8px', maxHeight: '24px'},
+  });
+  
+  // Prepare the legend labels
+  var labels = ui.Panel({
+    widgets: [
+      ui.Label(vis.min, {margin: '4px 8px'}),
+      ui.Label(
+          (vis.max / 2),
+          {margin: '4px 8px', textAlign: 'center', stretch: 'horizontal'}),
+      ui.Label(vis.max, {margin: '4px 8px'})
+    ],
+    layout: ui.Panel.Layout.flow('horizontal')
+  });
+    
+  // Prepare the panel to be returned
+  return ui.Panel({
+    widgets: [title, colorBar, labels],
+    style: {position: 'bottom-center', padding: '8px 15px'},
+  });    
+}
+
+function makeColorBarParams(palette) {
+  return {
+    bbox: [0, 0, 1, 0.1],
+    dimensions: '100x10',
+    format: 'png',
+    min: 0,
+    max: 1,
+    palette: palette,
+  };
+}
