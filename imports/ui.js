69f3d66ac52dafe7067a59daff21d1c7674df30b
rzupko <rzupko@mtu.edu>
2022-12-09 19:28:32 +0000

    Updated imports/ui.js (added 1 line, deleted 1 line).

--- imports/ui.js
+++ imports/ui.js
@@ -216,7 +216,7 @@ function addSpecies(year, species) {
 
 function updateLayers(data, visualization, label) {
   var layer = ui.Map.Layer(environmental.select('total_rainfall'), visual.viz_rainfall, 'Total Annual Rainfal, ' + year + ' (CHIRPS/PENTAD)');  
-  print(layer)
+  print(layer.getName())
 }
 
 // Add a layer to the map with the GMS outlined
