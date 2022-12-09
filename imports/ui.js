0084873244df0098d3d3ac07fefd5bb2bb531fb4
rzupko <rzupko@mtu.edu>
2022-12-09 20:29:58 +0000

    Updated imports/ui.js (added 1 line, deleted 1 line).

--- imports/ui.js
+++ imports/ui.js
@@ -30,7 +30,7 @@ exports.prepareUI = function() {
   Map.drawingTools().setShown(false);
   
   // Add the tool panel with the controls
-  toolPanel = ui.Panel([
+  var toolPanel = ui.Panel([
       ui.Label('Greater Mekong Subregion: Malaria Vector Risk Assessment',
         { fontSize: '1.5em', fontWeight: 'bold'}),
       ui.Label('Risk assessment is based upon environmental envelope for the Anopheles species selected and proximity to human development in the given year.'),
