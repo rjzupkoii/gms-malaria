cacec0ff8450aaddc3d69437c195c21af8c8a252
rzupko <rzupko@mtu.edu>
2022-12-09 21:59:21 +0000

    Updated imports/ui.js (added 1 line).

--- imports/ui.js
+++ imports/ui.js
@@ -143,6 +143,7 @@ function reset() {
   // Clear any layers that are present
   if (Map.layers().length() > 0) {
     for (var ndx = Map.layers().length(); ndx >= 0; ndx--) {
+      print(ndx)
       var layer = Map.layers().get(ndx);
       Map.remove(layer);
     }
