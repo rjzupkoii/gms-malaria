6c11a95e9d9adcbb2682053c58d413e646dbe628
rzupko <rzupko@mtu.edu>
2022-12-11 05:18:53 +0000

    Updated imports/widgets.js (added 1 line).

--- imports/widgets.js
+++ imports/widgets.js
@@ -71,6 +71,7 @@ exports.createDiscreteLegend = function(titleText, categories) {
   
   // Add the categories and return
   for (var key in categories) {
+    print(key)
     panel.add(makeRow(categories[key], key));
   }
   return panel;
