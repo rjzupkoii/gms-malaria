70e61719bd999c47c20162eb4b2a0c6dce8676cd
rbz5100 <rbz5100@psu.edu>
2023-01-10 17:18:35 +0000

    Updated imports/ui.js (added 2 lines, deleted 2 lines).

--- imports/ui.js
+++ imports/ui.js
@@ -254,8 +254,8 @@ function setSpecies(year, species) {
   
   // Create the export tasks
   storage.exportRaster(intermediate.select('days_outside_bounds'), g_year + '_' + species.species.replace(' ', '_') + '_days_outside_bounds');
-  storage.exportRaster(habitat, g_year + '_' + species.species + '_habitat');
-  storage.exportRaster(risk, g_year + '_' + species.species + '_risk');
+  // storage.exportRaster(habitat, g_year + '_' + species.species + '_habitat');
+  // storage.exportRaster(risk, g_year + '_' + species.species + '_risk');
 }
 
 // Calculate and add the year specific environment data to the map
