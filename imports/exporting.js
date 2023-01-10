ce48d59887fcafbd49acb1b6dabe8ca3bef4187c
rbz5100 <rbz5100@psu.edu>
2023-01-10 17:17:45 +0000

    Updated imports/exporting.js (deleted 2 lines).

--- imports/exporting.js
+++ imports/exporting.js
@@ -33,8 +33,6 @@ exports.exportEnvironmental = function(environmental, year) {
 
 // Create the export task for the raster using the given description.
 exports.exportRaster = function(raster, description) {
-  print(description.replace(' ', '_'))
-  
   Export.image.toDrive({
     image: raster,
     region: shapefile.getGms(),
