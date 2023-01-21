88a59fc42116ff54505fda30c5109f01b493948e
rbz5100 <rbz5100@psu.edu>
2023-01-21 01:32:14 +0000

    Updated imports/ui.js (added 1 line, deleted 1 line).

--- imports/ui.js
+++ imports/ui.js
@@ -57,7 +57,7 @@ exports.prepareUI = function() {
       ui.Label('This Earth Engine App is part of a research project at the Center for Infectious Disease Dynamics (CIDD), Pennsylvania State University. Data sources, source code, and project documentation can be found on GitHub.'),
       ui.Label('GMS Malaria at GitHub').setUrl('https://github.com/rjzupkoii/gms-malaria'),
     ], 
-    'flow', { 'width' : '350em' });
+    'flow', { 'width' : '50em' });
   ui.root.widgets().add(toolPanel);
 };
 
