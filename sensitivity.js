2cce80ef53bab24687151a8c36a4fec51d214166
rbz5100 <rbz5100@psu.edu>
2023-01-25 21:37:00 +0000

    Updated sensitivity.js (added 2 lines, deleted 1 line).

--- sensitivity.js
+++ sensitivity.js
@@ -3,5 +3,6 @@
  *
  * Script to queue the processing tasks for sensivity analysis.
  */
+var mosquitoes = require('users/rzupko/gms-malaria:assets/mosquitoes.js');
 
 var years = ee.List.sequence(2001, 2022);
