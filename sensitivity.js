c4663be3a8731092ef51adba1cc1a283a5f8099b
rbz5100 <rbz5100@psu.edu>
2023-01-25 21:54:27 +0000

    Updated sensitivity.js (added 16 lines, deleted 1 line).

--- sensitivity.js
+++ sensitivity.js
@@ -5,4 +5,19 @@
  */
 var mosquitoes = require('users/rzupko/gms-malaria:assets/mosquitoes.js');
 
-var years = ee.List.sequence(2001, 2022);
+var years = generateList(2001, 2022, 1);
+for (var ndx in years) {
+  for (var key in mosquitoes) {
+    print(years[ndx], mosquitoes[key].species);
+  }
+}
+
+// Generate the full 
+function generateList(first, last, step) {
+  var result = [first];
+  while (first < last) {
+    first += step;
+    result.push(first);
+  }
+  return result;
+}
