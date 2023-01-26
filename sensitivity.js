4ee6dde9636b2fec8b8d345cc33a0302f9966aa2
rbz5100 <rbz5100@psu.edu>
2023-01-26 18:18:55 +0000

    Updated sensitivity.js (added 15 lines, deleted 18 lines).

--- sensitivity.js
+++ sensitivity.js
@@ -17,24 +17,21 @@ generateJobs();
 
 // Main entry point for the script, generates the full list of jobs to be 
 // run on Earth Engine to conduct the sensitivity analysis.
-function generateJobs() {
-  // Generate the list of years and itterte on it
-  var years = generateList(2001, 2022, 1);
-  for (var ndx in years) {
-    queueEnvironmentalJob(years[ndx]);
+function generateJobs(year) {
+  // Start by queuing the environmental jobs
+  queueEnvironmentalJob(years[ndx]);
+  
+  // Iterate on all of the mosquitoes
+  for (var key in mosquitoes) {
     
-    // Iterate on all of the mosquitoes
-    for (var key in mosquitoes) {
-      
-      // Generate the list of deviations to test for sensitivity
-      var mosquito = mosquitoes[key];
-      var deviations = generateList(mosquito.tempMeanSD[0], mosquito.tempMeanSD[1], 0.25);
-      if (deviations[0] !== 0) {
-        deviations.push(0.0);
-      }
-      for (var ndy in deviations) {
-        queueVectorJob(years[ndx], mosquitoes[key], deviations[ndy]);
-      }
+    // Generate the list of deviations to test for sensitivity
+    var mosquito = mosquitoes[key];
+    var deviations = generateList(mosquito.tempMeanSD[0], mosquito.tempMeanSD[1], 0.25);
+    if (deviations[0] !== 0) {
+      deviations.push(0.0);
+    }
+    for (var ndy in deviations) {
+      queueVectorJob(years[ndx], mosquitoes[key], deviations[ndy]);
     }
   }
 }
