ed45dd69f5e3e94ea4e2fe18a8c4397a36418962
rbz5100 <rbz5100@psu.edu>
2023-01-27 01:07:57 +0000

    Updated imports/ml.js (added 1 line, deleted 1 line).

--- imports/ml.js
+++ imports/ml.js
@@ -66,7 +66,7 @@ exports.getReferenceImage = function(satellite) {
     .filterDate('2020-01-01', '2020-12-31');    
 
   var image = p125_r50.merge(p132_r42).merge(p129_r43).merge(p127_r51);
-  var image = image.map(processing.maskClouds).median();  
+  image = image.map(processing.maskClouds).median();  
   print(image);
   return image;
 };
