4c815eeda238fa95fb559b6c62d99d6fd67a9b31
rjzupkoii <rjzii@yahoo.com>
2023-01-30 15:09:26 -0500

    + Missing jobs pathway

--- python/gmsMalaria.py
+++ python/gmsMalaria.py
@@ -15,45 +15,8 @@ import sys
 from imports.progress import progressBar
 
 
-def iterate(wrapper, year):
-    # Load the analysis to be run
-    import imports.jsConversion as conversion
-    mosquitoes = conversion.load_mosquitoes()    
-    analysis, size = generate_analysis(mosquitoes)
-
-    # Prepare the progress bar
-    status = 0; size += 1
-    progressBar(status, size)
-
-    # Start by setting the year
-    wrapper.set_year(year)
-    wrapper.queue_environment()
-
-    # Update the progress bar, note we just want to indicate progress so it 
-    # doesn't have to be accurate
-    status += 1
-    progressBar(status, size)
-
-    # Iterate through the analysis set
-    for key in analysis:
-        wrapper.set_vector(mosquitoes[key])
-        for scalar in analysis[key]:
-            wrapper.queue_vector(scalar)
-            
-            # Note the progress
-            status += 1
-            progressBar(status, size)
-
-
 # Generate the sensitivity analysis tasks for the mosquitoes can be loaded 
-def generate_analysis(mosquitoes = None):
-
-    # Load the mosquitoes from the JavaScript if we weren't provided them this
-    # gives a bit of flexibility for command line processing
-    if mosquitoes is None:
-        import imports.jsConversion as conversion
-        mosquitoes = conversion.load_mosquitoes()
-
+def generate_analysis(mosquitoes):
     analysis = {}; size = 0
     for key in mosquitoes:
         analysis[key] = []
@@ -74,20 +37,107 @@ def generate_analysis(mosquitoes = None):
     return analysis, size
 
 
+def generate_missing(mosquitoes, path, year):
+    # Start by generating the full analysis set
+    analysis, _ = generate_analysis(mosquitoes)
+
+    # Now parse these into the expected file prefixes
+    prefixes = {}
+    for key in analysis:
+        name = mosquitoes[key]['species'].replace(' ', '_').replace('.', '')
+        for value in analysis[key]:
+            prefix = "{}_{}_{}".format(year, name, value)
+            prefixes[prefix] = key, value
+    
+    # Scan the files in the directory
+    for file in os.listdir(path):
+        for key in prefixes.keys():
+            if key in file:
+                del prefixes[key]
+                break
+    
+    # Return the missing jobs
+    return prefixes
+
+
+def iterate(wrapper, year):
+    # Load the analysis to be run
+    import imports.jsConversion as conversion
+    mosquitoes = conversion.load_mosquitoes()    
+    analysis, size = generate_analysis(mosquitoes)
+
+    # Prepare the progress bar
+    status = 0; size += 1
+    progressBar(status, size)
+
+    # Start by setting the year
+    wrapper.init()
+    wrapper.set_year(year)
+    wrapper.queue_environment()
+
+    # Update the progress bar, note we just want to indicate progress so it 
+    # doesn't have to be accurate
+    status += 1
+    progressBar(status, size)
+
+    # Iterate through the analysis set
+    for key in analysis:
+        wrapper.set_vector(mosquitoes[key])
+        for scalar in analysis[key]:
+            wrapper.queue_vector(scalar)
+            
+            # Note the progress
+            status += 1
+            progressBar(status, size)
+
+
+def iterate_missing(wrapper, path, year):
+    # Load the analysis to be run
+    import imports.jsConversion as conversion
+    mosquitoes = conversion.load_mosquitoes()
+    jobs = generate_missing(mosquitoes, path, year)
+
+    # Return if there is nothing to do
+    if len(jobs) == 0:
+        print('No jobs are missing from {}'.format(path))
+        return
+
+    # We've got missing jobs, start by setting the year
+    wrapper.init()
+    wrapper.set_year(year)
+
+    # Prepare the status bar
+    status = 0
+    progressBar(status, len(jobs.keys()))
+
+    # Iterate through the analysis set
+    for key in jobs:
+        job = jobs[key]
+        wrapper.set_vector(mosquitoes[job[0]])
+        wrapper.queue_vector(job[1])
+        
+        # Note the progress
+        status += 1
+        progressBar(status, len(jobs.keys()))
+
+
 def main(args):   
     # Prepare the processor
     import imports.eeWrapper as eeWrapper
     wrapper = eeWrapper.gmsEEWrapper()
-    wrapper.init()
-
+    
     # Start queuing the jobs
-    iterate(wrapper, int(args.year))
+    if args.missing:
+        iterate_missing(wrapper, args.missing, int(args.year))
+    else:
+        iterate(wrapper, int(args.year))    
     print("Queued Jobs: ", wrapper.get_count())
 
 
 if __name__ == '__main__':
     # Parse the parameters
     parser = argparse.ArgumentParser()
+    parser.add_argument('-m', action='store', dest='missing', help='Find the missing results in the given directory')
     parser.add_argument('-y', action='store', dest='year', required=True, help='The year to run the sensitivity analysis for')
     args = parser.parse_args()
 
