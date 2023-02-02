40043d37093a89755309c9cbf458c9bdc046674d
rbz5100 <rbz5100@psu.edu>
2023-02-02 15:11:19 +0000

    Updated imports/ml.js (added 10 lines, deleted 3 lines).

--- imports/ml.js
+++ imports/ml.js
@@ -63,8 +63,15 @@ exports.getReferenceImage = function(satellite) {
     .filter(ee.Filter.and(
       ee.Filter.eq('WRS_PATH', 127),
       ee.Filter.eq('WRS_ROW', 51)))
-    .filterDate('2020-01-01', '2020-12-31');    
-
-  var image = p125_r50.merge(p132_r42).merge(p129_r43).merge(p127_r51);
+    .filterDate('2020-01-01', '2020-12-31');
+    
+  // 
+  var p128_r49 = ee.ImageCollection(satellite.collection)
+    .filter(ee.Filter.and(
+      ee.Filter.eq('WRS_PATH', 128),
+      ee.Filter.eq('WRS_ROW', 49)))
+    .filterDate('2020-01-01', '2020-12-31');
+  
+  var image = p125_r50.merge(p132_r42).merge(p129_r43).merge(p127_r51).merge(p128_r49);
   return image.map(processing.maskClouds).median();  
 };
