b31688130aead4952fb3cd60c062b3659a7a61ff
rzupko <rzupko@mtu.edu>
2023-02-02 17:31:12 +0000

    Updated imports/ui.js (added 1 line, deleted 1 line).

--- imports/ui.js
+++ imports/ui.js
@@ -62,7 +62,7 @@ exports.prepareUI = function() {
       ui.Label(' '), ui.Label(' '),
       ui.Label('This Earth Engine App is part of a research project at the Center for Infectious Disease Dynamics (CIDD), Pennsylvania State University. Data sources, source code, and project documentation can be found on GitHub.'),
       ui.Label('GMS Malaria at GitHub').setUrl('https://github.com/rjzupkoii/gms-malaria'),
-      ui.Label('Deployed ' + )
+      ui.Label('Deployed ' + DEPLOYED);
     ], 
     'flow', { 'width' : PANEL_WIDTH });
   ui.root.widgets().add(toolPanel);
