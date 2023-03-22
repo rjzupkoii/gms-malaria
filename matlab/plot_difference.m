259e2370b70e6b9de463d2bc2786b8ac7a8c679f
rjzupkoii <rjzii@yahoo.com>
2023-03-22 15:52:28 -0400

    + landcover plot

--- matlab/plot_difference.m
+++ matlab/plot_difference.m
@@ -8,6 +8,10 @@ clear;
 habitat_heatmap( '../python/out/2-5km_habitat.csv', 2.5 * 2.5, 'Habitat (1km vs 2.5km)', 'habitat vs. 2.5km.png');
 habitat_heatmap( '../python/out/5km_habitat.csv', 5 * 5, 'Habitat (1km vs 5km)', 'habitat vs. 5km.png');
 
+% Create the heatmaps for the landcover
+landcover_heatmap( '../python/out/2-5km_landcover.csv', 2.5 * 2.5, 'Landcover (1km vs 2.5km)', 'landcover vs. 2.5km.png');
+landcover_heatmap( '../python/out/5km_landcover.csv', 5 * 5, 'Landcover (1km vs 2.5km)', 'landcover vs. 5km.png');
+
 function [] = habitat_heatmap(filename, scaling, title, out)
     % Load the data for 1km
     REFERENCE = '../python/out/1km_habitat.csv';
@@ -51,4 +55,43 @@ function [] = habitat_heatmap(filename, scaling, title, out)
     print('-dpng', '-r300', out);
     clf;
     close;
+end
+
+function [] = landcover_heatmap(filename, scaling, title, out)    
+    LABELS = {'Snow'; 'Shadow'; 'Water'; 'Forest'; 'Vegetation'; 'Vegetation / Scrub'; 'Barren'; 'Development'; 'Agricultural'};
+    
+    % Load the data for 1km
+    REFERENCE = '../python/out/1km_landcover.csv';
+    ref = readmatrix(REFERENCE);
+    
+    % Note the years, then remove the label columns and year row
+    years = ref(1:1, 2:end);
+    ref = ref(2:end, 2:end);
+    
+    % Load the comparision data, remove the label colums and year row
+    comp = readmatrix(filename);
+    comp = comp(2:end, 2:end);
+    
+    % Apply the scaling to the comparision data
+    comp = comp * scaling;
+    
+    % Calculate the percent difference
+    pd = ((comp - ref) ./ ref) * 100.0;  
+    
+    % Plot the heatmap
+    h = heatmap(pd);
+    caxis([-max(max(abs(pd))) max(max(abs(pd)))]);
+    h.Colormap = [flipud(h.Colormap); h.Colormap];
+    h.CellLabelFormat = '%0.3f%%';
+    h.XDisplayLabels = years;
+    h.YDisplayLabels = LABELS;
+    h.Title = title;
+
+    % Save the heatmap
+    graphic = gca;
+    graphic.FontSize = 12;
+    set(gcf, 'Position',  [0, 0, 2560, 1440]);
+    print('-dpng', '-r300', out);
+    clf;
+    close;    
 end
