4c92bd79aeeeb63a4dfaf8dbbc1a0fc7af513631
rzupko <rzupko@mtu.edu>
2023-03-28 15:38:00 +0000

    Updated bounds.js (added 12 lines, deleted 3 lines).

--- bounds.js
+++ bounds.js
@@ -1,5 +1,14 @@
 
 
-  // Load the data set
-  var temperature = ee.ImageCollection('MODIS/061/MOD11A1')
-    .filterDate(year + '-01-01', year + '-12-31');
+var shapefile = require('users/rzupko/gms-malaria:assets/shapefiles.js');
+
+// Setup our variables
+var gms = shapefile.getGms();
+var year = 2020;
+
+// Load the data set
+var temperature = ee.ImageCollection('MODIS/061/MOD11A1')
+  .filterDate(year + '-01-01', year + '-12-31');
+
+
+
