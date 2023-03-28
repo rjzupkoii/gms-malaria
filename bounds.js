d1156f40f5398eb1b7551199457d63def1803890
rzupko <rzupko@mtu.edu>
2023-03-28 15:42:39 +0000

    Updated bounds.js (added 7 lines, deleted 4 lines).

--- bounds.js
+++ bounds.js
@@ -9,7 +9,7 @@ var year = 2020;
 // Load the data set
 var temperature = ee.ImageCollection('MODIS/061/MOD11A1')
   .filterDate(year + '-01-01', year + '-12-31')
-  .select('LST_Night_1km')
+  .select('LST_Night_1km');
 temperature = temperature.map(function(image) { return })
 temperature = temperature.map(function(image) { return image.clip(gms); });
 
